20dc7d7fa62a177298f49223a9f0dc5e8557cef2
Topmula <clivemulambia1@gmail.com>
2026-06-13 22:16:55 +0200

    Update submitOTP.js

--- api/submitOTP.js
+++ api/submitOTP.js
@@ -2,17 +2,15 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   if (req.method === "OPTIONS") return res.status(204).end();
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
   try {
     const { collectionId, otp } = req.body;
-    const response = await fetch(`https://sandbox.lenco.co/access/v2/collections/${collectionId}/otp`, {
+    const response = await fetch(`https://api.lenco.co/access/v2/collections/${collectionId}/otp`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 993bed87f9d592566a6cce2cefd79363d1b7e95af3e1e6642b294ce5fc8c59f6",
+        "Authorization": "Bearer 22d74db1cc1957c484797085f15c893d0ca856a17fc1c8af576dba99d5193925",
       },
       body: JSON.stringify({ otp }),
     });
